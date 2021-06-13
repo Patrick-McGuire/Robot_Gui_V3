8abb1c032be88d6bcd8d863f8624bdb176418813
Patrick-McGuire <patrick.manson.mcguire@gmail.com>
2021-06-13 10:41:23 -0400

    test

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
 
-    QPushButton button("Hello world !");
+    QPushButton button("Hello wafsdorld !");
     button.show();
 
     return app.exec();
