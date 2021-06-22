f88756783ded5b9b543d6fafa97c736f8bc283bd
Nathan McGuire <nathan.e.mcguire@gmail.com>
2021-06-22 09:49:42 -0400

    better TestClient.py

--- Networking/TestClient.py
+++ Networking/TestClient.py
@@ -11,27 +11,32 @@ passDict = {
     "KEY3": "afsd",
 }
 
-s = socket.socket()
-s.connect(('127.0.0.1', 1254))
+IP_ADDRESS = "192.168.0.8"
+img = False
 
-while True:
-    passDict["KEY1"] = random.randint(0, 100)
-    passDict["KEY2"] = random.randint(0, 100) / 10
-    passDict["KEY3"] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
-    bytesToSend = bytearray(json.dumps(passDict).encode())
-    bytesToSend.insert(0, 3)
-    s.send(bytesToSend)
-    time.sleep(.05)
+if __name__ == '__main__':
+    s = socket.socket()
+    s.connect((IP_ADDRESS, 1254))
 
-# s = socket.socket()
-# s.connect(('127.0.0.1', 1254))
-# cam = cv2.VideoCapture(0)
-#
-# while True:
-#     ret_val, img = cam.read()
-#     is_success, im_buf_arr = cv2.imencode(".jpg", img)
-#     bytesIN = im_buf_arr.tobytes()
-#     bytesNEW = bytearray(bytesIN)
-#     bytesNEW.insert(0, 4)
-#     cv2.waitKey(1)
-#     s.send(bytesNEW)
+    if not img:
+        while True:
+            passDict["KEY1"] = random.randint(0, 100)
+            passDict["KEY2"] = random.randint(0, 100) / 10
+            passDict["KEY3"] = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+            bytesToSend = bytearray(json.dumps(passDict).encode())
+            print(bytesToSend)
+            bytesToSend.insert(0, 3)
+            s.send(bytesToSend)
+            time.sleep(0.1)
+    else:
+        cam = cv2.VideoCapture(0)
+
+        while True:
+            ret_val, img = cam.read()
+            is_success, im_buf_arr = cv2.imencode(".jpg", img)
+            bytesIN = im_buf_arr.tobytes()
+            bytesNEW = bytearray(bytesIN)
+            bytesNEW.insert(0, 4)
+            cv2.waitKey(1)
+            s.send(bytesNEW)
+            time.sleep(0.05)
