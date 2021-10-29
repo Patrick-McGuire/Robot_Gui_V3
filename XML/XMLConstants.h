470c176370976e51775fd56b047f2fa0eb8b1168
Patrick-McGuire <patrick.manson.mcguire@gmail.com>
2021-10-29 10:33:29 -0400

    Started XML

---
+++ XML/XMLConstants.h
@@ -0,0 +1,28 @@
+//
+// Created by patrick on 10/29/21.
+//
+
+#ifndef ROBOT_GUI_V3_XMLCONSTANTS_H
+#define ROBOT_GUI_V3_XMLCONSTANTS_H
+
+//// XML Attributes ////
+#define xmlWidthATR "width"
+#define xmlHeightATR "height"
+#define xmlTypeATR "type"
+#define xmlTitleATR "title"
+#define xmlThemeATR "theme"
+#define xmlXPosATR "x"
+#define xmlYPosATR "y"
+#define xmlHiddenATR "hidden"
+#define xmlDraggableATR "draggable"
+
+//// XML Tags ////
+#define xmlWindowTag "window"
+#define xmlWidgetCollectionTag "widgetCollection"
+#define xmlWidgetTag "widget"
+
+//// XML value constants ////
+#define xmlMaxConst "max"
+#define xmlAutoConst "auto"
+
+#endif //ROBOT_GUI_V3_XMLCONSTANTS_H
