f27f2506ba138bc27ab65de3f923d86b9126eb1b
Patrick-McGuire <patrick.manson.mcguire@gmail.com>
2022-04-04 21:02:29 -0400

    Doxygen

--- Interface/ServerInterface.h
+++ Interface/ServerInterface.h
@@ -15,28 +15,54 @@
 
 class CoreGui;
 
+/**
+ * @class ServerInterface
+ * Server that the GUI receives data from
+ *
+ * @author Patrick McGuire (Patrick-McGuire)
+ */
 class ServerInterface : public QTcpServer, public BaseInterface {
 Q_OBJECT
 public:
+    /**
+     * Constructor
+     * @param parent QObject parent
+     * @param _port Port to host on
+     */
     explicit ServerInterface(QObject *parent, int _port);
 
+    /**
+     * Sets the widget data structure to impute data to, and initializes things that use it
+     * @param _widgetData
+     */
     void setWidgetData(WidgetData *_widgetData) override;
 
+    /**
+     * Starts the server running
+     */
     void startServer();
 
 public slots:
+    /**
+     * Handles a incoming connection
+     */
     void incomingConnection();
 
+    /**
+     * Runs when ever the server is posted to
+     */
     void receiveData();
 
 signals:
+    /**
+     * Signal to emit when we have new data
+     */
     void newData();
 
 private:
     DataInput *dataInput;
     InternalJson::SharedPtr output = InternalJson::create(InternalJson::map_t);
     int port;
-
 };
 
 
